24a4587942fa7d64344a1793db6d56a774c529d6
Michal <michalogloza97@gmail.com>
2020-11-17 15:15:22 +0100

    weeia calendar access test

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello World!"
+
+
+@app.route('/calendar')
+def get_calendar():
+    return requests\
+        .get('http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=2020&miesiac=10&lang=1').content
 
 
 if __name__ == '__main__':
